8d7a81c729de90b402a8345e7e3e3447c1f3e1da
Adriano <adrianogalesso@outlook.com>
2015-08-14 19:25:13 -0300

    Added the Benchmark class

---
+++ Benchmark.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace toarray.utils
+{
+    public class Benchmark : IDisposable
+    {
+        private Stopwatch _watch;
+        private string _name;
+
+        public static Benchmark Start(string name)
+        {
+            return new Benchmark(name);
+        }
+
+        private Benchmark(string name)
+        {
+            _name = name;
+            _watch = new Stopwatch();
+            _watch.Start();
+        }
+
+        #region IDisposable implementation
+
+        public void Dispose()
+        {
+            _watch.Stop();
+            Console.WriteLine("{0} Total seconds: {1}", _name, _watch.Elapsed.TotalSeconds);
+        }
+
+        #endregion
+    }
+}
